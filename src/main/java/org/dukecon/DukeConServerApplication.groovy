6c1770db9f1d941e31bea18bf55d5acdbd5f8299
Falk Sippach <falk.sippach@oio.de>
2015-05-06 21:22:32 +0200

    initial spring boot setup

---
+++ src/main/java/org/dukecon/DukeConServerApplication.groovy
@@ -0,0 +1,12 @@
+package org.dukecon
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DukeConServerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run DukeConServerApplication, args
+    }
+}
---
+++ src/main/java/org/dukecon/ServletInitializer.groovy
@@ -0,0 +1,13 @@
+package org.dukecon
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
+
+class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		application.sources(DukeConServerApplication)
+	}
+
+}
