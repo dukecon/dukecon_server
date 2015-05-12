bb6b2f0e764252d32f0c1b0b2ab4e622fc8b4ef8
Gerd Aschemann <gerd@aschemann.net>
2015-05-12 14:48:29 +0200

    A first simple (i.e., hard coded) implementation of /meta

--- impl/src/main/java/org/dukecon/DukeConServerApplication.groovy
+++ impl/src/main/java/org/dukecon/DukeConServerApplication.groovy
@@ -2,8 +2,10 @@ package org.dukecon
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan ("org.dukecon.server.impl")
 class DukeConServerApplication {
 
     static void main(String[] args) {
