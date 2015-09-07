30f4d9198a88ecf4788b8d5a598a8177a97c3754
Christofer Dutz <christofer.dutz@codecentric.de>
2015-09-07 22:07:07 +0200

    - Refactored the PUT and DELETE method for adding and removing single preferences

--- impl/src/main/java/org/dukecon/server/service/AbstractPreferencesService.groovy
+++ impl/src/main/java/org/dukecon/server/service/AbstractPreferencesService.groovy
@@ -75,7 +75,7 @@ abstract class AbstractPreferencesService {
         // Add new userPreferences and update existing ones
         userPreferences.each {UserPreference up->
             Preference p = preferencesByTalk[up.talkId] ?: new Preference(principalId : principalId, talkId : up.talkId)
-            p = preferencesRepository.save(p)
+            preferencesRepository.save(p)
         }
 
         return Response.status(Response.Status.CREATED).build()
@@ -83,27 +83,26 @@ abstract class AbstractPreferencesService {
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response addPreferences (List<UserPreference> userPreferences) {
+    public Response addPreference (UserPreference userPreference) {
         String principalId = getAuthenticatedPrincipalId()
         if (!principalId) {
             return Response.status(Response.Status.NOT_FOUND).build()
         }
 
         log.debug ("Adding preferences for '{}'", principalId)
-        userPreferences.each { UserPreference userPreference ->
-            // Check if this preference was already created.
-            Collection<Preference> preferences = preferencesRepository.findByPrincipalIdAndTalkId(
-                    principalId, userPreference.talkId)
-            // If it doesn't exist yet, add it.
-            if((preferences == null) || preferences.empty) {
-                Preference p = new Preference(principalId : principalId, talkId : userPreference.talkId)
-                preferencesRepository.save(p)
-            } else {
-                // Well actually we tried to create something that's already
-                // there ... this is actually a conflict, but not a bad one
-                // let's leave it to the client do decide.
-                return Response.status(Response.Status.CONFLICT).build()
-            }
+
+        // Check if this preference was already created.
+        Collection<Preference> preferences = preferencesRepository.findByPrincipalIdAndTalkId(
+                principalId, userPreference.talkId)
+        // If it doesn't exist yet, add it.
+        if((preferences == null) || preferences.empty) {
+            Preference p = new Preference(principalId : principalId, talkId : userPreference.talkId)
+            preferencesRepository.save(p)
+        } else {
+            // Well actually we tried to create something that's already
+            // there ... this is actually a conflict, but not a bad one
+            // let's leave it to the client do decide.
+            return Response.status(Response.Status.CONFLICT).build()
         }
 
         return Response.status(Response.Status.CREATED).build()
@@ -111,28 +110,27 @@ abstract class AbstractPreferencesService {
 
     @DELETE
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response removePreferences (List<UserPreference> userPreferences) {
+    public Response removePreference (UserPreference userPreference) {
         String principalId = getAuthenticatedPrincipalId()
         if (!principalId) {
             return Response.status(Response.Status.NOT_FOUND).build()
         }
 
         log.debug ("Removing preferences for '{}'", principalId)
-        userPreferences.each { UserPreference userPreference ->
-            // Actually this should only return max one element, but I
-            // don't quite understand the mechanisms behind this service.
-            Collection<Preference> preferences = preferencesRepository.findByPrincipalIdAndTalkId(
-                    principalId, userPreference.talkId)
-            if((preferences != null) && !preferences.empty) {
-                preferences.each { Preference preference ->
-                    preferencesRepository.delete(preference.id)
-                }
-            } else {
-                // If we try to remove something that wasn't found,
-                // it's not a real problem, but by returning "not found"
-                // the client can react on it.
-                return Response.status(Response.Status.NOT_FOUND).build()
+
+        // Actually this should only return max one element, but I
+        // don't quite understand the mechanisms behind this service.
+        Collection<Preference> preferences = preferencesRepository.findByPrincipalIdAndTalkId(
+                principalId, userPreference.talkId)
+        if((preferences != null) && !preferences.empty) {
+            preferences.each { Preference preference ->
+                preferencesRepository.delete(preference.id)
             }
+        } else {
+            // If we try to remove something that wasn't found,
+            // it's not a real problem, but by returning "not found"
+            // the client can react on it.
+            return Response.status(Response.Status.NOT_FOUND).build()
         }
 
         return Response.status(Response.Status.OK).build()
