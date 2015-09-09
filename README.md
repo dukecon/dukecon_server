5a142ccb7833281a9d9be8b37e241947683b4482
Falk Sippach <falk@jug-da.de>
2015-09-09 11:45:38 +0200

    h2 database web console integrated

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 
 * API: [![Dependency Status API](https://www.versioneye.com/user/projects/5552099d06c318a32a0000c5/badge.svg?style=flat)](https://www.versioneye.com/user/projects/5552099d06c318a32a0000c5)
 * Impl: [![Dependency Status Impl](https://www.versioneye.com/user/projects/555209a206c3183055000123/badge.svg?style=flat)](https://www.versioneye.com/user/projects/555209a206c3183055000123)
+
+## DB
+
+* im Moment H2, später wahrscheinlich PostgreSQL
+* DB-Konsole für die H2: http://localhost:8080/console
--- impl/src/main/java/org/dukecon/DukeConServerApplication.groovy
+++ impl/src/main/java/org/dukecon/DukeConServerApplication.groovy
@@ -1,9 +1,13 @@
 package org.dukecon
 
+import org.h2.server.web.WebServlet
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.embedded.ServletRegistrationBean
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 @ComponentScan ("org.dukecon.server")
@@ -13,4 +17,12 @@ class DukeConServerApplication {
     static void main(String[] args) {
         SpringApplication.run DukeConServerApplication, args
     }
+
+    @Bean
+    public ServletRegistrationBean h2WebServlet() {
+        def registration = new ServletRegistrationBean(new WebServlet(), '/console/*')
+        return registration
+    }
 }
+
+
