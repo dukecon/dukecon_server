eea29fc71f0a7187a5ca008474cc9103100d570d
Gerd Aschemann <gerd@aschemann.net>
2015-09-18 01:06:50 +0200

    Moved ServletInitializer

--- impl/src/main/java/org/dukecon/server/impl/ServletInitializer.groovy
+++ impl/src/main/java/org/dukecon/server/impl/ServletInitializer.groovy
@@ -1,12 +1,18 @@
-package org.dukecon
+package org.dukecon.server.impl
 
+import groovy.util.logging.Slf4j
+
+import org.dukecon.DukeConServerApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.web.SpringBootServletInitializer
 
+
+@Slf4j
 class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		log.debug ("Configuring ServletInitializer")
 		application.sources(DukeConServerApplication)
 	}
 
