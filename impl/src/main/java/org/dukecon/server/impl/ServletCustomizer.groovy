c49ce585ddcd8ff8aa26b91f569d0438bc8c7c53
Gerd Aschemann <gerd@aschemann.net>
2015-09-18 01:08:48 +0200

    Set correct MIME Type for cache manifest

---
+++ impl/src/main/java/org/dukecon/server/impl/ServletCustomizer.groovy
@@ -0,0 +1,21 @@
+package org.dukecon.server.impl
+
+import groovy.util.logging.Slf4j
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer
+import org.springframework.boot.context.embedded.MimeMappings
+import org.springframework.stereotype.Component
+
+@Component
+@Slf4j
+public class ServletCustomizer implements EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+		log.debug ("Adding manifest mime mapping!")
+        MimeMappings mappings = container.mimeMappings
+        mappings.add("manifest","text/cache-manifest")
+        container.setMimeMappings(mappings)
+    }
+}
