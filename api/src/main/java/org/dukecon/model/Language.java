ffea1387c5a62aff70dcf392bcb8a0bd9d935797
Falk Sippach <falk@jug-da.de>
2015-10-08 22:00:03 +0200

    changed I18n message key from Language to String

--- api/src/main/java/org/dukecon/model/Language.java
+++ api/src/main/java/org/dukecon/model/Language.java
@@ -16,5 +16,5 @@ import java.util.Map;
 public class Language {
     private String id;
     private Integer order;
-    private Map<Language, String> names = new HashMap<>();
+    private Map<String, String> names = new HashMap<>();
 }
