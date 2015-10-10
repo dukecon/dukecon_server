a5232eb15b30a4f837d3b031154692b3a8fad640
Gerd Aschemann <gerd@aschemann.net>
2015-10-10 15:00:21 +0200

    Added KeyCloak Configuration service

--- impl/src/main/java/org/dukecon/server/impl/JerseyConfig.java
+++ impl/src/main/java/org/dukecon/server/impl/JerseyConfig.java
@@ -2,7 +2,13 @@ package org.dukecon.server.impl;
 
 import javax.ws.rs.ApplicationPath;
 
-import org.dukecon.server.service.*;
+import org.dukecon.server.service.ConferenceService;
+import org.dukecon.server.service.FiltersService;
+import org.dukecon.server.service.KeycloakConfigService;
+import org.dukecon.server.service.MetaService;
+import org.dukecon.server.service.NoAuthPreferencesServices;
+import org.dukecon.server.service.PreferencesService;
+import org.dukecon.server.service.TalkService;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +23,7 @@ public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         register(ConferenceService.class);
         register(MetaService.class);
+        register(KeycloakConfigService.class);
         register(TalkService.class);
         register(PreferencesService.class);
         register(NoAuthPreferencesServices.class);
