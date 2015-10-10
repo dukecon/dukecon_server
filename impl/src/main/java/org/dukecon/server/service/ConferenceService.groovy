698707135ebf7142e5a476b7bebbb3e63c299282
Falk Sippach <falk@jug-da.de>
2015-10-10 12:18:43 +0200

    added conference id to resource path

--- impl/src/main/java/org/dukecon/server/service/ConferenceService.groovy
+++ impl/src/main/java/org/dukecon/server/service/ConferenceService.groovy
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component
 import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
@@ -14,32 +15,38 @@ import javax.ws.rs.core.Response
  * @author Falk Sippach, falk@jug-da.de, @sippsack
  */
 @Component
-@Path("conference")
+@Path("conferences")
 @Produces(MediaType.APPLICATION_JSON)
 class ConferenceService {
     @Inject
     JavalandDataProvider talkProvider;
 
     @GET
-    public Response getConference() {
+    public Response getConferences() {
+        return Response.ok().entity([talkProvider.conference.id]).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getConference(@PathParam("id") String id) {
         return Response.ok().entity(talkProvider.conference).build();
     }
 
     @GET
-    @Path("speakers")
-    public Response getSpeakers() {
+    @Path("{id}/speakers")
+    public Response getSpeakers(@PathParam("id") String id) {
         return Response.ok().entity(talkProvider.conference.speakers).build();
     }
 
     @GET
-    @Path("talks")
-    public Response getTalks() {
+    @Path("{id}/events")
+    public Response getTalks(@PathParam("id") String id) {
         return Response.ok().entity(talkProvider.conference.talks).build();
     }
 
     @GET
-    @Path("metadata")
-    public Response getMeta() {
+    @Path("{id}/metadata")
+    public Response getMeta(@PathParam("id") String id) {
         return Response.ok().entity(talkProvider.conference.metaData).build();
     }
 }
