9a73a35c876324e945b2f55e861cc7b1f2d0c3b5
Gerd Aschemann <gerd@aschemann.net>
2015-12-07 22:35:53 +0100

    Avoid mapping of test server to fixed port ...

--- impl/src/test/java/org/dukecon/server/gui/DetailPageTest.java
+++ impl/src/test/java/org/dukecon/server/gui/DetailPageTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DukeConServerApplication.class)
-@WebIntegrationTest({"server.port=8080", "management.port=0"})
+@WebIntegrationTest({"server.port=0", "management.port=0"})
 public class DetailPageTest extends AbstractPageTest<GivenStartPage, WhenStartPage, ThenDetailPage> {
 
     @Test
