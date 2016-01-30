7750f491a068a990af2c04429fbd4f94c96fb494
Alexander Schwartz <alexander.schwartz@gmx.net>
2016-01-30 14:14:12 +0100

    use Java version provided by JAVA_HOME (cherry picked from commit 1615b16)

--- xvfb.sh
+++ xvfb.sh
@@ -1,6 +1,6 @@
-#/bin/bash -x
+#!/usr/bin/env bash -x
 # this is a wrapper for Xvfb to be able to run tests in headless CI environments
 # Use 
 #   mvn -Djvm="./xvfb.sh"
 # so that Surefire runs your tests in a Xvfb wrapped Java process
-/usr/bin/xvfb-run /usr/bin/java $@
+/usr/bin/xvfb-run $JAVA_HOME/bin/java $@
