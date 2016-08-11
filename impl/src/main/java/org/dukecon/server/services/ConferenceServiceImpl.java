2a7053045d0308218208b7b3ece22eae88978957
Christofer Dutz <christofer.dutz@codecentric.de>
2016-08-11 17:00:43 +0200

    Updated the model to correctly link redundant instances

--- impl/src/main/java/org/dukecon/server/services/ConferenceServiceImpl.java
+++ impl/src/main/java/org/dukecon/server/services/ConferenceServiceImpl.java
@@ -1,17 +1,13 @@
 package org.dukecon.server.services;
 
-import org.dukecon.model.Conference;
-import org.dukecon.model.Event;
-import org.dukecon.model.Language;
+import org.dukecon.model.*;
 import org.dukecon.server.conference.ConferenceDataProvider;
 import org.dukecon.services.ConferenceService;
 import org.springframework.flex.remoting.RemotingDestination;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by christoferdutz on 02.08.16.
@@ -63,6 +59,71 @@ public class ConferenceServiceImpl implements ConferenceService {
             for(Event event : conference.getEvents()) {
                 updateLanguageIds(event.getLanguage());
             }
+
+            Map<String, Audience> audienceMap = new HashMap<>();
+            Map<String, EventType> eventTypeMap = new HashMap<>();
+            Map<String, Language> languageMap = new HashMap<>();
+            Map<String, Location> locationMap = new HashMap<>();
+            Map<String, Speaker> speakerMap = new HashMap<>();
+            Map<String, Track> trackMap = new HashMap<>();
+            Map<String, Event> eventMap = new HashMap<>();
+            for(Audience audience : conference.getAudiences()) {
+                audience.setConference(conference);
+                audienceMap.put(audience.getId(), audience);
+            }
+            for(EventType eventType : conference.getEventTypes()) {
+                eventType.setConference(conference);
+                eventTypeMap.put(eventType.getId(), eventType);
+            }
+            for(Language language : conference.getLanguages()) {
+                language.setConference(conference);
+                languageMap.put(language.getId(), language);
+            }
+            conference.setDefaultLanguage(languageMap.get(conference.getDefaultLanguage().getId()));
+            for(Location location : conference.getLocations()) {
+                location.setConference(conference);
+                locationMap.put(location.getId(), location);
+            }
+            for(Speaker speaker : conference.getSpeakers()) {
+                speaker.setConference(conference);
+                speakerMap.put(speaker.getId(), speaker);
+            }
+            for(Track track : conference.getTracks()) {
+                track.setConference(conference);
+                trackMap.put(track.getId(), track);
+            }
+            for(Event event : conference.getEvents()) {
+                event.setConference(conference);
+                if(event.getAudience() != null) {
+                    event.setAudience(audienceMap.get(event.getAudience().getId()));
+                }
+                if(event.getTrack() != null) {
+                    event.setTrack(trackMap.get(event.getTrack().getId()));
+                }
+                if(event.getLanguage() != null) {
+                    event.setLanguage(languageMap.get(event.getLanguage().getId()));
+                }
+                if(event.getLocation() != null) {
+                    event.setLocation(locationMap.get(event.getLocation().getId()));
+                }
+                if(event.getType() != null) {
+                    event.setType(eventTypeMap.get(event.getType().getId()));
+                }
+                List<Speaker> speakers = new LinkedList<>();
+                for(Speaker speaker : event.getSpeakers()) {
+                    speakers.add(speakerMap.get(speaker.getId()));
+                }
+                event.setSpeakers(speakers);
+                eventMap.put(event.getId(), event);
+            }
+            for(Speaker speaker : conference.getSpeakers()) {
+                List<Event> events = new LinkedList<>();
+                for(Event event : speaker.getEvents()) {
+                    events.add(eventMap.get(event.getId()));
+                }
+                speaker.setEvents(events);
+            }
+
             conferences.add(conference);
         }
         return conferences;
