93061aa4ca99d41c361c6fb5d4af07860f3f2cf7
Gerd Aschemann <gerd@aschemann.net>
2017-05-28 14:12:50 +0200

    Enable parameterization of conferences configuration yml file

--- impl/src/main/java/org/dukecon/server/conference/ConferencesConfiguration.groovy
+++ impl/src/main/java/org/dukecon/server/conference/ConferencesConfiguration.groovy
@@ -16,7 +16,8 @@ import java.time.LocalDate
 class ConferencesConfiguration {
 
     static ConferencesConfiguration fromFile(String classpathName, Map<String, Object> allProperties) {
-        new ConferencesConfiguration(conferences: new Yaml(new YamlDateTimeConstructor()).load(new org.springframework.core.io.ClassPathResource(classpathName).inputStream).collect {
+        new ConferencesConfiguration(conferences: new Yaml(new YamlDateTimeConstructor()).load(
+                new org.springframework.core.io.ClassPathResource(classpathName).inputStream).collect {
             new Conference(substitutePlaceHolder(it, allProperties))
         })
     }
