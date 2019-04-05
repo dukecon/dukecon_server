097a8e8984a5ac3fd1e5402e15bd5f98350d0c7b
Gerd Aschemann <gerd@aschemann.net>
2019-04-05 20:17:38 +0200

    Fix local FileBackup tests
    Found out after some time that our new DSL provider (Telekom)
    forwards you to a navigation helper if you try to access an
    unknown domain (WTF):
    
      http://navigationshilfe.t-online.de/dtag/dns/results?tag=dnsep02r200200&url=www.dukecon-gibts-nicht.org&q=&opt=no&var=1

--- repositories/src/test/java/org/dukecon/server/repositories/FileBackuperSpec.groovy
+++ repositories/src/test/java/org/dukecon/server/repositories/FileBackuperSpec.groovy
@@ -4,38 +4,41 @@ import spock.lang.Specification
 
 class FileBackuperSpec extends Specification {
 
-    private String destDir
+    private String destDirName
 
     void setup() {
         def random = new Random()
-        destDir = "target/testoutput/FileBackuperSpec_${random.nextInt(1000)}"
-        assert !new File(destDir).exists()
+        destDirName = "target/testoutput/FileBackuperSpec_${random.nextInt(1000)}"
+        File destDir = new File(destDirName)
+        destDir.mkdirs()
+        assert destDir.exists()
     }
 
     void "Backup text content in a file"() {
         when:
-        def file = FileBackuper.of('some content', destDir, 'some_content.txt')
+        File file = FileBackuper.of('some content', destDirName, 'some_content.txt')
 
         then:
         assert file.exists()
-        assert file.getAbsolutePath().contains(destDir.tokenize('/').last())
+        assert file.getAbsolutePath().contains(destDirName.tokenize('/').last())
         assert file.text == 'some content'
     }
 
     void "Backup web url in a file"() {
         when:
-        def file = FileBackuper.of('http://www.dukecon.org'.toURL(), destDir, 'dukecon.org.html')
+        File file = FileBackuper.of('https://www.dukecon.org'.toURL(), destDirName, 'dukecon.org.html')
 
         then:
         assert file.exists()
-        assert file.getAbsolutePath().contains(destDir.tokenize('/').last())
+        assert file.getAbsolutePath().contains(destDirName.tokenize('/').last())
         assert file.text.contains('DukeCon Project')
     }
+
     void "Invalid web url won't backup a file"() {
         when:
-        def file = FileBackuper.of('http://www.dukecon-gibts-nicht.org'.toURL(), destDir, 'gibtsnicht.html')
+        File file = FileBackuper.of('https://www.dukecon-gibts-nicht.org'.toURL(), destDirName, 'gibtsnicht.html')
 
         then:
-        assert !file
+        assert !(file?.exists())
     }
 }
