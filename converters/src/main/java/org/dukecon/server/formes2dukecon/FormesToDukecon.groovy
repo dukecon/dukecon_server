02f60d7778b7c6c7bb57820ee5f12be3853a7abf
Gerd Aschemann <gerd@aschemann.net>
2019-04-30 00:33:56 +0200

    Add usage() for new converter main class

--- converters/src/main/java/org/dukecon/server/formes2dukecon/FormesToDukecon.groovy
+++ converters/src/main/java/org/dukecon/server/formes2dukecon/FormesToDukecon.groovy
@@ -2,6 +2,7 @@ package org.dukecon.server.formes2dukecon
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 import org.dukecon.model.Conference
 import org.dukecon.server.conference.ConferencesConfiguration
 import org.dukecon.server.conference.SpeakerImageService
@@ -11,8 +12,23 @@ import org.dukecon.server.repositories.RawDataResources
 
 
 @CompileStatic
+@Slf4j
 class FormesToDukecon {
+
+    private static void usage(String msg = null, int exitCode = 0) {
+        if (msg) {
+            log.error("Error calling '{}': {}", FormesToDukecon.class, msg)
+        }
+        log.info("Usage: {} <conferenceUrl>", FormesToDukecon.class)
+        if (exitCode) {
+            System.exit(exitCode)
+        }
+    }
+
     public static void main(String[] args) {
+        if (args.length != 1) {
+            usage("Wrong number of arguments", 1)
+        }
         ConferencesConfiguration conferencesConfiguration = ConferencesConfiguration.fromFile(args[0], [:], false)
         conferencesConfiguration.conferences.each { ConferencesConfiguration.Conference conferenceConfig ->
             RawDataResources rawDataResources = RawDataResources.of(conferenceConfig)
