3cd8404cf7c591439bd64fdfb69fb8cdfe4dc86f
Falk Sippach <falk.sippach@oio.de>
2019-05-09 21:50:30 +0200

    creation of init.json

--- converters/src/main/java/org/dukecon/server/formes2dukecon/FormesToDukecon.groovy
+++ converters/src/main/java/org/dukecon/server/formes2dukecon/FormesToDukecon.groovy
@@ -10,6 +10,8 @@ import org.dukecon.server.repositories.ConferenceDataExtractor
 import org.dukecon.server.repositories.RawDataMapper
 import org.dukecon.server.repositories.RawDataResources
 
+import java.time.format.DateTimeFormatter
+
 
 @CompileStatic
 @Slf4j
@@ -44,9 +46,44 @@ class FormesToDukecon {
                             .newInstance(conferenceConfig, rawDataMapper, new DummySpeakerImageService())
             Conference conference = conferenceDataExtractor.conference
             ObjectMapper objectMapper = new ObjectMapper()
-            File json = new File("conference-${conferenceConfig.id}.json")
-            objectMapper.writeValue(json, conference);
-            println json.absolutePath
+            File conferenceJson = new File("static/${conferenceConfig.conference}/${conferenceConfig.year}/${conferenceConfig.id}.json")
+            conferenceJson.getParentFile().mkdirs()
+            objectMapper.writeValue(conferenceJson, conference)
+            log.info("Created {}", conferenceJson.absolutePath)
+
+            File initJson = new File("static/${conferenceConfig.conference}/${conferenceConfig.year}/init.json")
+            objectMapper.writeValue(initJson, getInitJsonContent(conferenceConfig))
+            log.info("Created {}", initJson.absolutePath)
         }
+    }
+
+    private static Map<String, Object> getInitJsonContent(ConferencesConfiguration.Conference c) {
+        final DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_DATE
+        [
+                id          : c.id,
+                name        : c.name,
+                year        : c.year,
+                url         : c.url,
+                homeUrl     : c.homeUrl,
+                homeTitle   : c.homeTitle,
+                imprint     : [
+                        de      : c.imprint.de,
+                        en      : c.imprint.en
+                ],
+                termsOfUse  : c.termsOfUse,
+                privacy     : c.privacy,
+                startDate   : dtf.format(c.startDate),
+                endDate     : dtf.format(c.endDate),
+                authEnabled : c.authEnabled,
+                admin       : "../rest/admin/${c.id}".toString(),
+                forceUpdate : "../rest/conferences/update/${c.id}".toString(),
+                conferences : "../rest/conferences/${c.id}".toString(),
+                events      : "../rest/eventsBooking/${c.id}".toString(),
+                keycloak    : "../rest/keycloak.json",
+                feedbackServer: [
+                        active: Boolean.valueOf(c.feedbackServer.active),
+                        timeSlotVisible: c.feedbackServer.timeSlotVisible as int
+                ]
+        ]
     }
 }
