be2ff7eee40a32e8fd9f3015bd71b2b993fce0b3
Falk Sippach <falk.sippach@oio.de>
2019-10-13 15:02:56 +0200

    added comment about deprecation

--- converters/src/main/java/org/dukecon/server/convert/DummySpeakerImageService.groovy
+++ converters/src/main/java/org/dukecon/server/convert/DummySpeakerImageService.groovy
@@ -2,6 +2,11 @@ package org.dukecon.server.convert
 
 import org.dukecon.server.conference.SpeakerImageService
 
+/**
+ * Will be obsolete as soon as conference provider will be disabled in dukecon_server implementation. This interface and
+ * its references can be removed when speaker images will be saved as static files.
+ */
+@Deprecated
 class DummySpeakerImageService implements SpeakerImageService {
     @Override
     Map<String, ImageWithName> getImages() {
