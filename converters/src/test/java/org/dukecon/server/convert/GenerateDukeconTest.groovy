851e831babf54c6ff5acef6832a241a23b88d821
Falk Sippach <falk.sippach@oio.de>
2020-02-24 02:04:18 +0100

    integration test implemented

--- converters/src/test/java/org/dukecon/server/convert/GenerateDukeconTest.groovy
+++ converters/src/test/java/org/dukecon/server/convert/GenerateDukeconTest.groovy
@@ -1,17 +1,30 @@
 package org.dukecon.server.convert
 
 
-import org.junit.Ignore
 import org.junit.Test
 
 /**
  * @author Falk Sippach, falk@jug-da.de, @sippsack
  */
 class GenerateDukeconTest {
-    @Ignore
     @Test
-    public void testMain() {
-        GenerateDukecon.main(['conferences-javaland.yml'] as String[])
+    void testMain() {
+        GenerateDukecon.main(['conferences-javaland.yml', '../resources/src/main/resources'] as String[])
+
+        String outputDir = 'htdocs/rest/javaland/2019/'
+        assert new File(outputDir).exists()
+        assert new File(outputDir + 'img/favicon.ico').exists()
+        assert new File(outputDir + 'styles.css').exists()
+        assert new File(outputDir + 'rest/conferences/javaland2019.json').exists()
+        new File(outputDir + 'rest/speaker/images').with {
+            assert it.isDirectory()
+            assert !it.list().toList().isEmpty()
+        }
+        assert new File(outputDir + 'rest/styles.css').exists()
+        assert new File(outputDir + 'rest/init.json').exists()
+        assert new File(outputDir + 'rest/image-resources.json').exists()
+
+        // TODO: test for keys in json files
     }
 
 
